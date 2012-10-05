df9c853ebbef6868d7af7a72b127fc8275090d20
Jack Bracken <contact@jacktrick.com>
2012-10-05 18:13:06 +0100

    Fixed all the things Sam broke. Muahaha. Ha.

--- account_create.sh
+++ account_create.sh
@@ -5,14 +5,18 @@ IN_FILE="users.csv"
 
 function email () {
     SUBJECT="NETSOC ACCOUNT CREATED"
-
     EMAIL=$STUDENT_NUMBER$MAIL_POSTFIX
-
     EMAILMESSAGE="email.txt"
 
     echo "Your account was created"> $EMAILMESSAGE
+    echo "" >> $EMAILMESSAGE
     echo "Username: $USERNAME" >>$EMAILMESSAGE
     echo "Password: $PASSWORD" >>$EMAILMESSAGE
+    echo "" >> $EMAILMESSAGE
+    echo "Login at $USERNAME@netsoc.com via SSH" >> $EMAILMESSAGE
+    echo "Access your webpage at $USERNAME.netsoc.com" >> $EMAILMESSAGE
+    echo "" >> $EMAILMESSAGE
+    echo "NETSOC Team" >> $EMAILMESSAGE
 
     /usr/bin/mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
 }
@@ -36,10 +40,14 @@ do
     if [ "$USER_EXISTS" -ne "0" ] ; then
         # USER EXISTS, CHANGE PASSWORD
         echo "$USERNAME:$PASSWORD" | chpasswd
-    else
+else
         # USER DOES NOT EXIST, CREATE USER
         echo "Creating user $USERNAME"
-        sudo useradd -m -p "$PASSWORD" "$USERNAME"
+        adduser -q $USERNAME
+        echo "$USERNAME:$PASSWORD" | chpasswd
+        mkdir /home/$USERNAME/public_html
+        echo "$USERNAME" > /home/$USERNAME/public_html/index.html
+        chown -R $USERNAME:$USERNAME /home/$USERNAME
     fi
     email $USERNAME $STUDENT_NUMBER $PASSWORD
     echo
